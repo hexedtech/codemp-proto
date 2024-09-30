403200d6bcd841a58e0e2863f115a08f1586e2d6
alemi <me@alemi.dev>
2024-09-30 23:51:33 +0200

    feat: slim down
    reduce tonic features, add feature flags to compile only server, client,
    both or neither

--- build.rs
+++ build.rs
@@ -1,14 +1,34 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	Ok(tonic_build::configure().compile(
-		&[
-			"proto/common.proto",
-			"proto/cursor.proto",
-			"proto/files.proto",
-			"proto/auth.proto",
-			"proto/session.proto",
-			"proto/workspace.proto",
-			"proto/buffer.proto",
-		],
-		&["proto"],
-	)?)
+	let server = {
+		#[cfg(feature = "server")] { true }
+		#[cfg(not(feature = "server"))] { false }
+	};
+
+	let client = {
+		#[cfg(feature = "client")] { true }
+		#[cfg(not(feature = "client"))] { false }
+	};
+
+	let transport = {
+		#[cfg(any(feature = "server", feature = "client"))] { true }
+		#[cfg(not(any(feature = "server", feature = "client")))] { false }
+	};
+
+	Ok
+		(tonic_build::configure()
+			.build_server(server)
+			.build_client(client)
+			.build_transport(transport)
+			.compile_protos(
+				&[
+					"proto/common.proto",
+					"proto/cursor.proto",
+					"proto/files.proto",
+					"proto/auth.proto",
+					"proto/session.proto",
+					"proto/workspace.proto",
+					"proto/buffer.proto",
+				],
+				&["proto"],
+			)?)
 }
